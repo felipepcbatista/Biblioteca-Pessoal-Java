b565c5c2ad899d01d6748bf55583792e85a98618
Felipe Paz Carvalho Batista <lipepaz23@gmail.com>
2025-09-07 16:38:29 -0300

    feat(repo): define LivroRepository interface

---
+++ src/main/java/biblioteca/infra/repo/LivroRepository.java
@@ -0,0 +1,14 @@
+package biblioteca.infra.repo;
+
+import biblioteca.domain.Livro;
+import java.util.List;
+import java.util.Optional;
+
+public interface LivroRepository{
+
+    Livro salvar (Livro livro);
+    Optional<Livro> buscarPorId (long id);
+    List<Livro> listar();
+    void removerLivro (long id);
+    long proximoId ();
+}
