93e46a50ca104e038b3435476d1e919ff89faa03
Felipe Paz Carvalho Batista <lipepaz23@gmail.com>
2025-09-08 09:55:43 -0300

    feat(repo): define UsuarioRepository interface

---
+++ src/main/java/biblioteca/infra/repo/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package biblioteca.infra.repo;
+
+import biblioteca.domain.Usuario;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository{
+    Usuario salvar (Usuario user);
+    Optional<Usuario> buscarPorId (long id);
+    List<Usuario> listar ();
+    long proximoId ();
+}
